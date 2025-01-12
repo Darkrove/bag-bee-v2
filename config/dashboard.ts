5e7a44246605d65663ad2dc60dd2b38f7bcfcc18
Sajjad Shaikh <samaralishaikh212@gmail.com>
2025-01-12 14:51:45 +0530

    change in loading screen and remove view invoice page

--- config/dashboard.ts
+++ config/dashboard.ts
@@ -18,7 +18,6 @@ export const sidebarLinks: SidebarNavItem[] = [
       { href: "/dashboard/sales-data", icon: "table", title: "Sales Data", disabled: true},
       { href: "/dashboard/customers", icon: "usersRound", title: "Customers", disabled: true},
       { href: "/dashboard/invoices", icon: "database", title: "Invoices", disabled: true}, 
-      { href: "/dashboard/view-invoice", icon: "search", title: "View Invoice"},
       {
         href: "/admin/orders",
         icon: "package",
