de3884b9d293a46ef35b7d1ad85975608d34ace6
Sajjad Shaikh <samaralishaikh212@gmail.com>
2025-01-12 15:07:17 +0530

    change in loading screen and remove view invoice page

--- app/(protected)/dashboard/view-invoice/[billid]/loading.tsx
+++ app/(protected)/dashboard/view-invoice/[billid]/loading.tsx
@@ -2,11 +2,14 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function ViewInvoiceLoading() {
   return (
-    <div className="mx-auto max-w-[85rem] px-4 py-8 sm:px-6 lg:px-8">
-      <div className="mx-auto flex flex-col space-y-4 sm:w-11/12 lg:w-3/4">
-        <Skeleton className="size-full h-10 rounded-xl" />
-        <Skeleton className="size-full rounded-xl border-t-[12px] border-primary" />
+    <section className="grid items-center gap-6">
+      <div className="mx-auto w-full max-w-[85rem] px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto flex flex-col space-y-4 sm:w-11/12 lg:w-3/4">
+          <Skeleton className="size-full h-28 rounded-xl" />
+          <Skeleton className="size-full h-[600px] rounded-xl border-t-[12px] border-primary" />
+          <Skeleton className="size-full h-28 rounded-xl" />
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
