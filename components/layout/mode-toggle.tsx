70db11a9b188fe9ae17cc056e87fdcd4a420dca3
Sajjad Shaikh <samaralishaikh212@gmail.com>
2025-09-18 12:12:17 +0530

    fix issue with transition

--- components/layout/mode-toggle.tsx
+++ components/layout/mode-toggle.tsx
@@ -1,18 +1,18 @@
-"use client";
+"use client"
 
-import { Icons } from "@/components/shared/icons";
+import { Icons } from "@/components/shared/icons"
 
-import { useTheme } from "next-themes";
-import * as React from "react";
+import { useTheme } from 'next-themes';
+import * as React from 'react';
 
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 
 export function ModeToggle() {
   const { setTheme, resolvedTheme } = useTheme();
 
   const handleThemeToggle = React.useCallback(
     (e?: React.MouseEvent) => {
-      const newMode = resolvedTheme === "dark" ? "light" : "dark";
+      const newMode = resolvedTheme === 'dark' ? 'light' : 'dark';
       const root = document.documentElement;
 
       if (!document.startViewTransition) {
@@ -22,27 +22,27 @@ export function ModeToggle() {
 
       // Set coordinates from the click event
       if (e) {
-        root.style.setProperty("--x", `${e.clientX}px`);
-        root.style.setProperty("--y", `${e.clientY}px`);
+        root.style.setProperty('--x', `${e.clientX}px`);
+        root.style.setProperty('--y', `${e.clientY}px`);
       }
 
       document.startViewTransition(() => {
         setTheme(newMode);
       });
     },
-    [resolvedTheme, setTheme],
+    [resolvedTheme, setTheme]
   );
 
   return (
     <Button
-      variant="outline"
-      size="icon"
-      className="group/toggle size-8"
+      variant='secondary'
+      size='icon'
+      className='group/toggle size-8'
       onClick={handleThemeToggle}
     >
       <Icons.sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Icons.moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      <span className="sr-only">Toggle theme</span>
+          <Icons.moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className='sr-only'>Toggle theme</span>
     </Button>
   );
 }
